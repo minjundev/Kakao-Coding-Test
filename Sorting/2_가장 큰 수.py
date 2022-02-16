015320eea3c664e35b3896f1e04a4ebc7df107d7
minjun <rapthon@naver.com>
2022-02-16 17:18:01 +0900

    sorting

--- Sorting/2_가장 큰 수.py
+++ Sorting/2_가장 큰 수.py
@@ -1,4 +1,4 @@
 def solution(numbers):
     numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*4, reverse=True)
+    numbers.sort(key=lambda x: x*3, reverse=True)
     return str(int(''.join(numbers)))
